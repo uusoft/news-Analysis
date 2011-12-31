ea64174a2c97272ea4965a4d8eb52c7157a57971
cuitao <cuitao@cuitao.sohu-inc.com>
2011-12-31 17:11:05 +0800

    refactoring mainDriver and all jobs to extends ToolJob, not finished yet

--- MainDriver2.java
+++ MainDriver2.java
@@ -1,6 +1,9 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
 import test.TestMapWritableJob;
 
 import lib.ToolJob;
@@ -28,11 +31,34 @@ public class MainDriver2 {
 		add("pref", pref.PrefJob.class);
 	}
 	
+	public static void main (String[] args) throws Exception {
+		MainDriver2 driver = new MainDriver2();
+		String className = args[0];
+		if (args[0].equalsIgnoreCase("-help")) {
+			driver.printHelp();
+			System.exit(0);
+		}
+		if (!driver.find(className)) {
+			System.out.println("do not have this class");
+			System.exit(0);
+		}
+		else {
+			String[] newArgs = new String[args.length-1];
+			for (int i=1; i<args.length; i++) {
+				newArgs[i-1] = args[i];
+			}
+			ToolRunner.run((Tool) driver.getClass(className).newInstance(), newArgs);
+
+		}
+	}
 	public void add(String className,Class<?> cls) {
 		classMap.put(className, cls);
 		
 	}
 	
+	public Class<?> getClass(String className) {
+		return classMap.get(className);
+	}
 	public void printHelp() {
 		for (Map.Entry<String, Class<?>> entry: classMap.entrySet()) {
 			System.out.println(entry.getKey());
